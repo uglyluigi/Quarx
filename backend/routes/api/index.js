1ebc92490512796d00860959a42edd589a26d39e
BillPepsiOfPepsiCo <5006946+BillPepsiOfPepsiCo@users.noreply.github.com>
2019-09-19 11:50:46 -0500

    Amend index to include EMS API hooks

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/blog-posts', require('./blog-posts'));
+router.use('/event-messenger', require('./event-messenger'));
 
 module.exports = router;
