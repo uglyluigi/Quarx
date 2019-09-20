55e890dea935989f43ab454d07b76ee227a66d9f
uglyluigi <5006946+uglyluigi@users.noreply.github.com>
2019-09-20 14:08:41 -0500

    Add EXTREMELY professional-looking comments to common file

--- backend/routes/api/common.js
+++ backend/routes/api/common.js
@@ -1,21 +1,46 @@
-function produce_update_response(response, out, postId) {
-    const {matchedCount, modifiedCount} = out;
-    console.log(`Matched count: ${matchedCount} | Modified count: ${modifiedCount}`);
+const ObjectId = require('mongodb').ObjectID;
 
-    if (modifiedCount == 1) {
-        response.status(200).json({message: `Post with id ${postId} has been modified.`});
-    } else if (modifiedCount == 0 && matchedCount == 1) { //If it was matched but not updated
-        response.status(304).json({message: `Post with id ${postId} was not modified.`});
-    } else { //If it wasn't even matched
-        response.status(404).json({
-            message: `Cannot modify post ${postId}: it does not exist.`,
-            sorry: false
-        });
+module.exports = {
+    /**
+     * Function used to simplify responding to update/delete requests.
+     * Takes the 'out' parameters from a mongoDB updateOnce requests and
+     * responds to them in kind.
+     *
+     * @param response the Express response object.
+     * @param out the 'out' parameters that contain the modified and matched counts for the DB query.
+     * @param objectId the ID of the DB object, used only to tailor the response.
+     */
+    produce_update_response: function (response, out, objectId) {
+        const {matchedCount, modifiedCount} = out;
+        console.log(`Matched count: ${matchedCount} | Modified count: ${modifiedCount}`);
+
+        if (modifiedCount == 1) {
+            response.status(200).json({message: `Post with id ${objectId} has been modified.`});
+        } else if (modifiedCount == 0 && matchedCount == 1) { //If it was matched but not updated
+            response.status(304).json({message: `Post with id ${objectId} was not modified.`});
+        } else { //If it wasn't even matched
+            response.status(404).json({
+                message: `Cannot modify post ${objectId}: it does not exist.`,
+                sorry: false
+            });
+        }
+    },
+
+    /**
+     * Function used to reduce boilerplate at endpoints that require a postId.
+     * Verifies them using ObjectId.isValid.
+     *
+     * @param response the response object. Only modified if the given objectId is invalid.
+     * @param objectId the objectId to attempt to validate.
+     * @returns {boolean} true if the objectId is valid; false if it's not. The given response object's status
+     * will be set to 400 when this function returns false.
+     */
+    validate_objid_and_respond: function (response, objectId) {
+        if (!ObjectId.isValid(objectId)) {
+            response.status(400).json({message: `The provided ID \'${objectId}\' is not a valid DB object ID.`});
+            return false;
+        } else {
+            return true;
+        }
     }
-}
-
-function validate_objid_and_respond(response, objectId) {
-
-}
-
-module.exports = produce_update_response;
+};
