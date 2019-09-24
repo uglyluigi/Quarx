60ae943a5a076cd4bab3bfa5900573cfc2b53a42
Brennan Forrest <5006946+uglyluigi@users.noreply.github.com>
2019-09-23 22:35:55 -0500

    🧹SWEEP SWEEP SWEEP🧹
    It is now dirtyn't (in my humble, incredibly valuable and clout-ful opinion)

--- backend/routes/api/login.js
+++ backend/routes/api/login.js
@@ -1,73 +1,99 @@
-import {handle_unauthorized_api_call} from "./common"
+import {handle_unauthorized_api_call, go_away_err} from "./common"
 
 const AccessUser = require('../../models/access-user');
 const passport = require('passport');
 const router = require('express').Router();
 
+/**
+ * POST router to /api/login/signup
+ *
+ * Probably not even going to be used,
+ * since there's only going to be 1 user
+ * allowed to log in to the site since
+ * we only want site admins to have special
+ * access.
+ */
 router.post('/signup', (request, response, next) => {
     if (handle_unauthorized_api_call(request, response)) {
         return response;
     }
 
-    AccessUser.register(new AccessUser({
-        username: request.body.username
-    }),
-        request.body.password, (err, user) => {
+    AccessUser.register(new AccessUser({username: request.body.username}), request.body.password, (err, user) => {
             if (err) {
-                response.statusCode = 500;
-                response.setHeader('Content-Type', 'application/json');
-                response.json({
-                    err: err
-                });
+                response.status(500).json({err: err});
             } else {
                 passport.authenticate('local')(request, response, () => {
-                    AccessUser.findOne({
-                        username: request.body.username
-                    }, (err, person) => {
-                        response.statusCode = 200;
-                        response.setHeader('Content-Type', 'application/json');
-                        response.json({
-                            success: true,
-                            status: "GOOD JOB"
-                        })
+                    AccessUser.findOne({username: request.body.username}, (err, person) => {
+                        go_away_err(err, response);
+
+                        if (!err) {
+                            response.status(200).json({
+                                message: "You\'ve been successfully logged in.",
+                                success: true,
+                                happy_4_u: true
+                            });
+                        }
                     })
-                })
+                });
             }
         });
+
+    return response;
 });
 
+/**
+ * POST router to /api/login/login (yep)
+ *
+ * Receives a username and password. Logs the user in if passport says it's OK.
+ *
+ * Body:
+ * {
+ *     username: "username",
+ *     password: "password"
+ * }
+ *
+ * Known issues:
+ * * Probably going to need another layer of auth of some kind since the request is made containing a plaintext
+ * password, making this susceptible to MITM attacks, however if we configure SSL correctly on the actual
+ * deployed site this might become a non-issue.
+ */
 router.post('/login', passport.authenticate('local'), (request, response) => {
     AccessUser.findOne({username: request.body.username}, (err, person) => {
         request.login(person, function (err, something) {
-
+            console.log("Login request received");
+            go_away_err(err, response);
         });
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'application/json');
-        response.json({
-            success: true,
-            status: "YOU DID IT!!!"
-        })
+
+        response.status(200).json({message: "You have been successfully logged in.", nice_one: true});
     });
+
+    return response;
 });
 
+/**
+ * GET router to /api/login/logout (lol).
+ * Destroys the user's session if one exists.
+ *
+ * Known issues:
+ * * Does not respond correctly when trying to log out without an active session. Always responds
+ * with the inner 200-returning expression, but this could be an issue with how Postman stores cookies.
+ * I don't know! Jeez!
+ */
 router.get('/logout', (request, response, next) => {
     if (request.session) {
         request.logout();
         request.session.destroy((err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                response.clearCookie('expression');
-                response.json({
-                    message: "You are successfully logged out!"
-                });
+            go_away_err(err);
+
+            if (!err) {
+                response.status(200).clearCookie('expression').json({message: "You have been logged out."});
             }
-        })
+        });
     } else {
-        const err = new Error("You are not logged in!");
-        err.status = 403;
-        next(err);
+        response.status(403).json({message: "You were not logged out (no session to destroy).", sorry: false});
     }
+
+    return response;
 });
 
 module.exports = router;
