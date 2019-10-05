df242034baac7abc79c29f8179d598280508ce6d
Brennan Forrest <5006946+uglyluigi@users.noreply.github.com>
2019-10-05 16:43:10 -0500

    Migrate from the mongo node client to the mongoose schema

--- backend/routes/api/event-messenger.js
+++ backend/routes/api/event-messenger.js
@@ -1,30 +1,48 @@
 import {DB_URI, DB_CLUSTER_NAME, BLOG_COLLECTION_NAME, EMS_COLLECTION_NAME} from "../../constants";
-
-const mongoose = require('mongoose');
+import {flatten_error_array} from './common';
 const router = require('express').Router();
 const EMSUser = require('../../models/ems-user.model');
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
 
 router.post('/', (request, response, next) => {
     console.log("POST to /event-messenger");
+    let new_user = new EMSUser({email: request.body.email, phone_number: request.body.phone_number});
 
-    const email = request.body.email;
-    const phone_number = request.body.phone_number;
+    new_user
+        .save()
+        .then(() => {
+            response.status(201).json({message: "EMS User successfully registered."});
+        }, (err) => {
+            let error_msgs = [];
 
-    if (!email) {
-        return response.status(400).json({message: "Event messenger entries must contain an email address."});
-    }
+            if (err.errors) {
+                let errors = err.errors;
 
-    MongoClient.connect(DB_URI)
-        .then(connection => {
-            connection.db(DB_CLUSTER_NAME)
-                .collection(EMS_COLLECTION_NAME)
-                .insert({email: email, phone_number: phone_number})
-                .then(out => {
-                    response.status(201).json({message: "EMS User successfully registered"});
-                })
-                .then(() => connection.close());
+                if (errors.email) {
+                    error_msgs[0] = errors.email.message;
+                }
+
+                if (errors.phone_number) {
+                    error_msgs[1] = errors.phone_number.message;
+                }
+
+                /**
+                 * A quick explanation of .filter(x => x)
+                 * I'm trying to exclude null elements from the error array when
+                 * sending it back in the response. undefined, null, NaN, etc. are
+                 * all falsey values when converted to booleans. The predicate
+                 * passed to .filter will only succeed if 'x' is a truthy value,
+                 * or basically any string value besides null or undefined
+                 * (with negligible exceptions in this case).
+                 */
+                response.status(400).json({err: error_msgs.filter(x => x)});
+            } else if (err.name === "MongoError") {
+                if (err.code === 11000) {
+                    response.status(409).json({err: "That email address or phone number already exists in the DB."});
+                } else {
+                    error_msgs[2] = "MongoError: " + err.errmsg;
+                    response.status(400).json({err: error_msgs.filter(x => x)})
+                }
+            }
         });
 
     return response;
