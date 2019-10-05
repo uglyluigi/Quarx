877d0b3b8db979e277fe52214d5d1dabda39b5e7
Brennan Forrest <5006946+uglyluigi@users.noreply.github.com>
2019-10-05 16:42:00 -0500

    Amend schema to verify emails and phone numbers

--- backend/models/ems-user.model.js
+++ backend/models/ems-user.model.js
@@ -1,18 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
 
 const EMSUserSchema = new Schema({
    email: {
        type: String,
        required: true,
        unique: true,
-       trim: true
+       trim: true,
+       validate: [validateEmail, "Invalid email address."],
    },
 
     phone_number: {
        type: String,
         required: false,
         unique: true,
-        trim: true
+        trim: true,
+        validate: [validatePhoneNumber, "Invalid phone number."],
     }
 });
+
+function validateEmail(value) {
+    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(value.toLowerCase());
+}
+
+function validatePhoneNumber(value) {
+    return phoneUtil.isValidNumber(phoneUtil.parse(value, 'US'));
+}
+
+const EMSUser = mongoose.model('ems-user', EMSUserSchema);
+module.exports = EMSUser;
