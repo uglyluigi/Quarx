5eb653f20bd557757b98a323fc9f215704731521
Flowborg <logan.fr33man@gmail.com>
2019-10-06 13:09:04 -0500

    snackbar integrated

--- frontend/src/components/mail.component.js
+++ frontend/src/components/mail.component.js
@@ -9,6 +9,18 @@ import {createMuiTheme} from "@material-ui/core/styles";
 import {ThemeProvider, withStyles} from "@material-ui/styles";
 import {PropTypes} from 'prop-types';
 import {getBaseUrl} from './../service';
+import clsx from 'clsx';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import InfoIcon from '@material-ui/icons/Info';
+import CloseIcon from '@material-ui/icons/Close';
+import {amber, green} from '@material-ui/core/colors';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import WarningIcon from '@material-ui/icons/Warning';
+import {makeStyles} from '@material-ui/core/styles';
+
 const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
 
 
@@ -77,9 +89,118 @@ const useStyles = theme => ({
         color: 'white'
     },
 });
+const useStyles1 = makeStyles(theme => ({
+    success: {
+        backgroundColor: green[600],
+    },
+    error: {
+        backgroundColor: theme.palette.error.dark,
+    },
+    info: {
+        backgroundColor: theme.palette.primary.main,
+    },
+    warning: {
+        backgroundColor: amber[700],
+    },
+    icon: {
+        fontSize: 20,
+    },
+    iconVariant: {
+        opacity: 0.9,
+        marginRight: theme.spacing(1),
+    },
+    message: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+}));
+const variantIcon = {
+    success: CheckCircleIcon,
+    warning: WarningIcon,
+    error: ErrorIcon,
+    info: InfoIcon,
+};
+
+function MySnackbarContentWrapper(props) {
+    const classes = useStyles1();
+    const {className, message, onClose, variant, ...other} = props;
+    const Icon = variantIcon[variant];
+
+    return (
+        <SnackbarContent
+            className={clsx(classes[variant], className)}
+            aria-describedby="client-snackbar"
+            message={
+                <span id="client-snackbar" className={classes.message}>
+          <Icon className={clsx(classes.icon, classes.iconVariant)}/>
+                    {message}
+        </span>
+            }
+            action={[
+                <IconButton key="close" aria-label="close" color="inherit" onClick={onClose}>
+                    <CloseIcon className={classes.icon}/>
+                </IconButton>,
+            ]}
+            {...other}
+        />
+    );
+}
+
+MySnackbarContentWrapper.propTypes = {
+    className: PropTypes.string,
+    message: PropTypes.string,
+    onClose: PropTypes.func,
+    variant: PropTypes.oneOf(['error', 'info', 'success', 'warning']).isRequired,
+};
+
+const useStyles2 = makeStyles(theme => ({
+    margin: {
+        margin: theme.spacing(1),
+    },
+}));
+
+export function CustomizedSnackbars() {
+    const classes = useStyles2();
+    const [open, setOpen] = React.useState(false);
+
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Button variant="outlined" className={classes.margin} onClick={handleClick}>
+                Open success snackbar
+            </Button>
+            <Snackbar
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                open={open}
+                autoHideDuration={6000}
+                onClose={handleClose}
+            >
+                <MySnackbarContentWrapper
+                    onClose={handleClose}
+                    variant="success"
+                    message="This is a success message!"
+                />
+            </Snackbar>
+        </div>
+    );
+}
 
 export class MailComponent extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
 
         this.handleUserInput = this.handleUserInput.bind(this);
@@ -99,7 +220,7 @@ export class MailComponent extends React.Component {
         }
     }
 
-    handleUserInput (e) {
+    handleUserInput(e) {
         e.preventDefault();
 
         const name = e.target.name;
@@ -110,7 +231,7 @@ export class MailComponent extends React.Component {
         })
     }
 
-    validateField (name, value) {
+    validateField(name, value) {
         let emailValid = this.state.emailValid;
         let phoneValid = this.state.phoneNumberValid;
 
@@ -138,7 +259,7 @@ export class MailComponent extends React.Component {
         this.setState({emailValid: emailValid, phoneNumberValid: phoneValid}, () => this.validateForm());
     }
 
-    onSubmit (e) {
+    onSubmit(e) {
         e.preventDefault();
         const axios = require('axios');
 
@@ -151,7 +272,7 @@ export class MailComponent extends React.Component {
         })
     }
 
-    onLoseFocus (e) {
+    onLoseFocus(e) {
         let emailError = this.state.emailError;
         let phoneNumberError = this.state.phoneNumberError;
         const target = e.target.name;
@@ -170,11 +291,11 @@ export class MailComponent extends React.Component {
         this.setState({emailError: emailError, phoneNumberError: phoneNumberError});
     }
 
-    validateForm () {
+    validateForm() {
         this.setState({formValid: this.state.emailValid && this.state.phoneNumberValid})
     }
 
-    render () {
+    render() {
         const {classes} = this.props;
 
         return (
@@ -187,7 +308,7 @@ export class MailComponent extends React.Component {
                         Sign up
                     </Typography>
                     <form className={classes.form} onSubmit={this.onSubmit}>
-                        <ThemeProvider theme={theme}>
+                        <ThemeProvider theme={theme} uiTheme={theme}>
                             <TextField
                                 onChange={this.handleUserInput}
                                 error={this.state.emailError}
@@ -248,6 +369,7 @@ export class MailComponent extends React.Component {
                             >
                                 Sign Up
                             </Button>
+                            <CustomizedSnackbars/>
                         </ThemeProvider>
                     </form>
                 </div>
