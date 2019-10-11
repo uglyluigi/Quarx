a41382e14ef72ee84ba32ab05c60afaadbec9857
Brennan Forrest <5006946+uglyluigi@users.noreply.github.com>
2019-10-11 14:23:55 -0500

    Lean on back end to supply images for carousel
    How did i figure this out so quickly

--- frontend/src/components/gallery.component.js
+++ frontend/src/components/gallery.component.js
@@ -1,45 +1,60 @@
 import React, {Component} from 'react';
 import {Col, Container, Row, Carousel} from "react-bootstrap";
+import {get_base_url_for_api_reqs} from "../common";
 
 export default class Gallery extends Component {
     constructor(props) {
         super(props);
 
         this.getPhotos.bind(this);
+        this.factoryItems.bind(this);
 
         this.state = {
-            num_photos: 0,
-            photos: []
+            images: [],
+            children: []
         };
     }
 
     getPhotos() {
         let axios = require('axios');
 
+        axios.get('http://localhost:5000/api/assets/carousel-images/')
+            .then(response => {
+                this.setState({images: response.data.images}, () => {
+                    console.log(this.state.images);
+                    this.factoryItems();
+                });
+            });
+    }
+
+    factoryItems() {
+        let children = [];
+
+        for (let i = 0; i < this.state.images.length; i++) {
+            children[i] =
+
+            <Carousel.Item>
+                <img className={"d-block w-100"} src={this.state.images[i]} alt='Uhhh'/>
+                <Carousel.Caption>
+                    <h3>Yee haw</h3>
+                    <p>Bepis</p>
+                </Carousel.Caption>
+            </Carousel.Item>;
+        }
+
+        console.log(children);
+        this.setState({children: children});
     }
 
     componentDidMount() {
-
+        this.getPhotos();
     }
 
     render() {
         return (
             <Container>
                 <Carousel>
-                    <Carousel.Item>
-                        <img className={"d-block w-100"} src={require('../assets/photos/1.jpg')} alt="First slide"/>
-                        <Carousel.Caption>
-                            <h3>Yeet haw</h3>
-                            <p>EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img className={"d-block w-100"} src={require('../assets/photos/2.jpeg')} alt="Second slide"/>
-                        <Carousel.Caption>
-                            <h3>Yeet haw</h3>
-                            <p>EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
+                    {this.state.children}
                 </Carousel>
             </Container>
         )
