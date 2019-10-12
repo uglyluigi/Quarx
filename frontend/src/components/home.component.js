7967fe9ced64b1a6a74bf139475564c6751eea8b
Flowborg <logan.fr33man@gmail.com>
2019-10-12 12:08:41 -0500

    renamed home.component.css to match other styles

--- frontend/src/components/home.component.js
+++ frontend/src/components/home.component.js
@@ -1,5 +1,5 @@
 import '../styles/material-fonts.css'
-import '../styles/home-component.css'
+import '../styles/home.component.css'
 import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 import {makeStyles} from '@material-ui/core/styles';
