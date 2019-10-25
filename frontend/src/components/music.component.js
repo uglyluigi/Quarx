13660397cb95a5685bb21a645eb478e9deb306e1
Flowborg <logan.fr33man@gmail.com>
2019-10-25 13:41:06 -0500

    updated video

--- frontend/src/components/music.component.js
+++ frontend/src/components/music.component.js
@@ -14,7 +14,7 @@ export default class Music extends Component {
                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen/>
 
-                    <iframe width="1375" height="787" src="https://www.youtube.com/embed/XrSKLibOw1g"
+                    <iframe width="1375" height="787" src="https://www.youtube.com/embed/wI5QQUsrlOY"
                             frameBorder="1"
                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen/>
