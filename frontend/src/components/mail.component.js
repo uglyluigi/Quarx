22d91290402d2d4346e4dd75b9e0e8c02723d449
Flowborg <logan.fr33man@gmail.com>
2019-10-28 15:04:28 -0500

    Mail page updated again

--- frontend/src/components/mail.component.js
+++ frontend/src/components/mail.component.js
@@ -408,88 +408,89 @@ export class MailComponent extends React.Component {
         const {classes} = this.props;
 
         return (
-            <Container maxWidth="xs">
-                <Row>
-                    <div>
-                        <p>Use the sign up form on the right to for email and/or text alerts for live performances from Quarx.<br/>
-                        Sign-up only requires a valid email address. A phone number can also be provided if you would
-                        prefer to receive alerts that way, or added later.
+            <Container>
+                <div className={classes.alignmentForm}>
+
+                    <form className={classes.form} onSubmit={this.onSubmit}>
+                        <div className={classes.alignmentItem}>
+                            <Avatar className={classes.avatar}>
+                                <MailOutlineIcon/>
+                            </Avatar>
+                            <Typography component="h1" variant="h5">
+                                Sign up
+                            </Typography>
+                        </div>
+                        <ThemeProvider theme={theme}>
+                            <TextField
+                                onChange={this.handleUserInput}
+                                error={this.state.emailError}
+                                onBlur={this.onLoseFocus}
+                                autoFocus
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                InputLabelProps={{
+                                    classes: {
+                                        root: classes.labelColor,
+                                        focused: classes.focused,
+                                    },
+                                }}
+                                InputProps={{
+                                    classes: {
+                                        root: classes.inputColor,
+                                        focused: classes.focused,
+                                        notchedOutline: classes.notchedOutline,
+                                    }
+                                }}
+                            />
+                            <TextField
+                                onChange={this.handleUserInput}
+                                error={this.state.phoneNumberError}
+                                onBlur={this.onLoseFocus}
+                                variant="outlined"
+                                margin="normal"
+                                fullWidth
+                                id="phoneNumber"
+                                label="Phone Number"
+                                name="phoneNumber"
+                                InputLabelProps={{
+                                    classes: {
+                                        root: classes.labelColor,
+                                        focused: classes.focused,
+                                    },
+                                }}
+                                InputProps={{
+                                    classes: {
+                                        root: classes.inputColor,
+                                        focused: classes.focused,
+                                        notchedOutline: classes.notchedOutline,
+                                    }
+                                }}
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="secondary"
+                                className={classes.submit}
+                                disabled={!this.state.formValid}
+                                onClick={this.handleClick}
+                            >
+                                Sign Up
+                            </Button>
+                            <CustomizedSnackbars variant={this.state.snackBarVariant}
+                                                 message={this.state.snackBarMessage} open={this.state.showSnackBar}/>
+                        </ThemeProvider>
+                    </form>
+                    <div className={classes.alignmentText}>
+                        <p>Use the sign up form to register for email and/or text alerts about live performances from<br/>
+                            Quarx. Sign-up requires a valid email address. At any time, a phone number can also be
+                            provided to receive alerts.
                         </p>
-                    </div>
-                    <div className={classes.alignment}>
-                        <Avatar className={classes.avatar}>
-                            <MailOutlineIcon/>
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
-                            Sign up
-                        </Typography>
-                        <form className={classes.form} onSubmit={this.onSubmit}>
-                            <ThemeProvider theme={theme}>
-                                <TextField
-                                    onChange={this.handleUserInput}
-                                    error={this.state.emailError}
-                                    onBlur={this.onLoseFocus}
-                                    autoFocus
-                                    variant="outlined"
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    InputLabelProps={{
-                                        classes: {
-                                            root: classes.labelColor,
-                                            focused: classes.focused,
-                                        },
-                                    }}
-                                    InputProps={{
-                                        classes: {
-                                            root: classes.inputColor,
-                                            focused: classes.focused,
-                                            notchedOutline: classes.notchedOutline,
-                                        }
-                                    }}
-                                />
-                                <TextField
-                                    onChange={this.handleUserInput}
-                                    error={this.state.phoneNumberError}
-                                    onBlur={this.onLoseFocus}
-                                    variant="outlined"
-                                    margin="normal"
-                                    fullWidth
-                                    id="phoneNumber"
-                                    label="Phone Number"
-                                    name="phoneNumber"
-                                    InputLabelProps={{
-                                        classes: {
-                                            root: classes.labelColor,
-                                            focused: classes.focused,
-                                        },
-                                    }}
-                                    InputProps={{
-                                        classes: {
-                                            root: classes.inputColor,
-                                            focused: classes.focused,
-                                            notchedOutline: classes.notchedOutline,
-                                        }
-                                    }}
-                                />
-                                <Button
-                                    type="submit"
-                                    fullWidth
-                                    variant="contained"
-                                    color="secondary"
-                                    className={classes.submit}
-                                    disabled={!this.state.formValid}
-                                    onClick={this.handleClick}
-                                >
-                                    Sign Up
-                                </Button>
-                                <CustomizedSnackbars variant={this.state.snackBarVariant}
-                                                     message={this.state.snackBarMessage} open={this.state.showSnackBar}/>
-                            </ThemeProvider>
-                        </form>
                     </div>
                 </div>
 
