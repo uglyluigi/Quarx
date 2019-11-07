9fb1ed96d9b8f8c7f749f928bacb3b6225eaf412
Flowborg <Logan.fr33man@gmail.com>
2019-11-07 06:00:48 -0600

    set max nav height and cleaned up

--- frontend/src/components/navbar.component.js
+++ frontend/src/components/navbar.component.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { MDBNavbar, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBIcon } from "mdbreact";
+import {MDBNavbar, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBIcon} from "mdbreact";
 import Link from "@material-ui/core/Link";
 import "../styles/navbar.component.css";
 import FacebookIcon from '@material-ui/icons/Facebook';
@@ -11,59 +11,51 @@ class NavBar extends Component {
     render() {
         return (
 
-                <MDBNavbar
-                    className="navbar fixed-top navbar-expand-lg navbar-dark"
+            <MDBNavbar
+                className="navbar fixed-top navbar-expand-lg navbar-dark"
                 style={{
                     backgroundColor: '#171717',
+                    maxHeight: '7vh',
                 }}>
-                        <MDBNavbarNav left>
-                            <MDBNavItem active>
-                                <MDBNavLink to="/">Home</MDBNavLink>
-                            </MDBNavItem>
-                            <MDBNavItem active>
-                                <MDBNavLink to="/music">Music</MDBNavLink>
-                            </MDBNavItem>
-                            <MDBNavItem active>
-                                <MDBNavLink to="/mail">Mail</MDBNavLink>
-                            </MDBNavItem>
-                            <MDBNavItem active>
-                                <MDBNavLink to="/gallery">Gallery</MDBNavLink>
-                            </MDBNavItem>
-                        </MDBNavbarNav>
-                        <MDBNavbarNav right>
-                            <MDBNavItem>
-                                <MDBNavLink className="waves-effect waves-light" to="#!">
-                                    <MDBIcon fab icon="twitter" />
-                                </MDBNavLink>
-                            </MDBNavItem>
-                            <MDBNavItem>
-                                <MDBNavLink className="waves-effect waves-light" to="#!">
-                                    <MDBIcon fab icon="google-plus-g" />
-                                </MDBNavLink>
-                            </MDBNavItem>
-                            <MDBNavItem>
-                                <Link
-                                    href={'https://www.facebook.com/Quarxband/'}>
-                                    <FacebookIcon className={"icon"}
-                                        color='primary'
-                                        style={{fontSize: '4.5vh'}}/>
-                                </Link>
-                                <Link
-                                    href={'https://twitter.com/quarxband?lang=en'}>
-                                    <TwitterIcon className={"icon"}
-                                        color='primary'
-                                        style={{fontSize: '4.5vh'}}/>
-                                </Link>
-                                <Link
-                                    href={'https://www.instagram.com/quarxband/'}>
-                                    <InstagramIcon className={"icon"}
-                                        color='primary'
-                                    style={{fontSize: '4.5vh'}}/>
-                                </Link>
-                            </MDBNavItem>
-                        </MDBNavbarNav>
-                </MDBNavbar>
+                <MDBNavbarNav left>
+                    <MDBNavItem active>
+                        <MDBNavLink to="/">Home</MDBNavLink>
+                    </MDBNavItem>
+                    <MDBNavItem active>
+                        <MDBNavLink to="/music">Music</MDBNavLink>
+                    </MDBNavItem>
+                    <MDBNavItem active>
+                        <MDBNavLink to="/mail">Mail</MDBNavLink>
+                    </MDBNavItem>
+                    <MDBNavItem active>
+                        <MDBNavLink to="/gallery">Gallery</MDBNavLink>
+                    </MDBNavItem>
+                </MDBNavbarNav>
+                <MDBNavbarNav right>
+                    <MDBNavItem>
+                        <Link
+                            href={'https://www.facebook.com/Quarxband/'}>
+                            <FacebookIcon className={"icon"}
+                                          color='primary'
+                                          style={{fontSize: '4.5vh'}}/>
+                        </Link>
+                        <Link
+                            href={'https://twitter.com/quarxband?lang=en'}>
+                            <TwitterIcon className={"icon"}
+                                         color='primary'
+                                         style={{fontSize: '4.5vh'}}/>
+                        </Link>
+                        <Link
+                            href={'https://www.instagram.com/quarxband/'}>
+                            <InstagramIcon className={"icon"}
+                                           color='primary'
+                                           style={{fontSize: '4.5vh'}}/>
+                        </Link>
+                    </MDBNavItem>
+                </MDBNavbarNav>
+            </MDBNavbar>
         );
     }
 }
+
 export default NavBar;
