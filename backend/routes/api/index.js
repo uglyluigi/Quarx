7c624a7c49f0eb754f39d8d6f36af7f694e53110
Brennan Forrest <5006946+uglyluigi@users.noreply.github.com>
2019-11-17 00:31:54 -0600

    Add test API to index

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,3 +1,5 @@
+import {IS_TEST_ENV} from "../../constants";
+
 const express = require('express');
 const router = express.Router();
 
@@ -5,5 +7,10 @@ router.use('/blog-posts', require('./blog-posts'));
 router.use('/event-messenger', require('./event-messenger'));
 router.use('/login', require('./login'));
 router.use('/assets', require('./assets'));
+
+if (IS_TEST_ENV) {
+    console.log("Enabling test APIs");
+    router.use('/test', require('./test'));
+}
 
 module.exports = router;
