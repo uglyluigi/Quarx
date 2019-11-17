dc009ced60e778689b5f158f6a4ff315aa2c9a60
Brennan Forrest <5006946+uglyluigi@users.noreply.github.com>
2019-11-17 00:34:40 -0600

    Add log messages for send grid worker

--- backend/workers/sendgrid-worker.js
+++ backend/workers/sendgrid-worker.js
@@ -1,3 +1,5 @@
+import {IS_TEST_ENV} from "../constants";
+
 require('dotenv').config();
 const sgMail = require('@sendgrid/mail');
 const EMSUser = require('../models/ems-user.model');
@@ -7,6 +9,10 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 function sendToAll(subject, html) {
     //Gets all users and send them the email
     EMSUser.find({}, (error, users) => {
+        if (users.length === 0) {
+            console.log('No email addresses to send emails to');
+        }
+
         for (let user of users) {
             html += `<br/>To unsubscribe, click <a href=\"http://localhost:5000/api/event-messenger/delete?email=${user.email}\">here</a>`;
             sendEmail(user.email, 'alerts@quarx.com', subject, html);
@@ -21,6 +27,10 @@ function sendEmail(to, from, subject, html) {
         subject: subject,
         html: html,
     };
+
+    if (IS_TEST_ENV) {
+        console.log(`Sending email to ${to}`);
+    }
 
     sgMail.send(msg);
 }
