4727bdc4c0ae146c743864d471c98091fe8e1f93
Brennan Forrest <5006946+uglyluigi@users.noreply.github.com>
2019-11-16 20:51:59 -0600

    add way for subscribed users to delete themselves from db

--- backend/workers/sendgrid-worker.js
+++ backend/workers/sendgrid-worker.js
@@ -8,6 +8,7 @@ function sendToAll(subject, html) {
     //Gets all users and send them the email
     EMSUser.find({}, (error, users) => {
         for (let user of users) {
+            html += `<br/>To unsubscribe, click <a href=\"http://localhost:5000/api/event-messenger/delete?email=${user.email}\">here</a>`;
             sendEmail(user.email, 'alerts@quarx.com', subject, html);
         }
     });
