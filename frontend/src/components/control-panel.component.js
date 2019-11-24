2a7712e2d25cdfdf038252d42cd767c3477ebd85
Brennan Forrest <5006946+uglyluigi@users.noreply.github.com>
2019-11-24 16:03:34 -0600

    Add (completely broken) html editor

--- frontend/src/components/control-panel.component.js
+++ frontend/src/components/control-panel.component.js
@@ -1,15 +1,52 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
-import Typography from '@material-ui/core/Typography';
+import {EditorState} from 'draft-js';
+import {Editor} from 'react-draft-wysiwyg';
+import {getBaseUrl} from '../common';
 
 export default class ControlPanel extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.componentDidMount = this.componentDidMount.bind(this);
+
+        this.state = {
+            doLoad: false
+        }
+    }
+
     render() {
-        return (
-            <Container maxWidth={"xs"}>
-                <Typography>
-                    <h1>Yeet haw</h1>
-                </Typography>
-            </Container>
-        );
+        if (this.state.doLoad) {
+            return (
+                <Editor
+                    editorState={EditorState.defaultEditorState}
+                    toolbarClassName={"HTMLEditorToolbar"}
+                    wrapperClassName={"HTMLEditorWrapper"}
+                    editorClassName={"HTMLEditor"}/>
+            );
+        } else {
+            return (<div/>);
+        }
+    }
+
+    componentDidMount() {
+        const axios = require('axios');
+        const config = {
+            headers: {
+                Authorization: `Bearer ${localStorage.token}`,
+            }
+        };
+
+        axios.get(getBaseUrl() + "/api/login/login", config).then(response => {
+            console.log(response);
+
+            if (response.status === 200) {
+                this.setState({doLoad: true});
+            } else {
+                this.setState({doLoad: false});
+            }
+        }, err => {
+            this.setState({doLoad: false});
+        });
     }
 }
